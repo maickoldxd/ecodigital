90897f975fbaac09a88e4741a7cc23d673c78e3d
Maickol <vasquez@fktech.net>
2024-08-02 17:06:52 -0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./pages/**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        eco: "#72ECA3",
+      },
+    },
+  },
+  plugins: [],
+};
